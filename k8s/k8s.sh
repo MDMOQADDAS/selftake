b9bc98ed5b520f6682f29ae299717661e954a964
MD MOQADDAS <69861558+MDMOQADDAS@users.noreply.github.com>
2024-05-13 09:27:20 +0530

    Update k8s.sh

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -15,4 +15,12 @@ alias k=kubectl
 
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
+
+curl -O https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+# Auto-detect the BGP IP address.
+            - name: IP
+              value: "autodetect"
+              ########## update this 
+            - name: IP_AUTODETECTION_METHOD
+              value: "interface=eth0"
 kubectl apply -f calico.yml
